cebad32b7e1ada6ff892eda7f094ab971ec82c9e
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-07-10 02:34:19 -0700

    adds css, goal status, and readme

--- frontend/src/components/GoalList.js
+++ frontend/src/components/GoalList.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+// css
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 class GoalList extends Component {
 
+
+handleClick = (e) => {
+  this.props.onUpdateStatus(e)
+}
+
+
 handleChange = (e) => {
-  this.props.onUpdateGoal(e, this.props.goal.id)
+  this.props.onUpdateGoal(e, this.props.goal)
 }
 
 handleSubmit = (e) => {
@@ -16,11 +30,17 @@ render() {
 const {title, description, status, steps } = this.props.goal
   return (
     <form onSubmit={(e) => this.handleSubmit(e)}>
+    <FormControl component="fieldset">
         <h2>{title}</h2>
         <p>{description}</p>
-        <textarea type="text" name="currentGoal" onChange={(e) => this.handleChange(e)} value={steps} />
-        <label name="status" onChange={this.handleChange}>{status}</label> 
-       <button type="submit">Submit</button>
+        <textarea type="text" name="steps" onChange={(e) => this.handleChange(e)} value={steps} />
+        <RadioGroup defaultValue={status} aria-label="gender" name="status">
+          <FormControlLabel name="status" value="Not Started" control={<Radio />} label="Not Started" onClick={(e) => this.handleClick(e)} />
+          <FormControlLabel name="status" value="In-Process" control={<Radio />} label="In-Process" onClick={(e) => this.handleClick(e)}/>
+          <FormControlLabel name="status" value="Complete" control={<Radio />} label="Complete" onClick={(e) => this.handleClick(e)}/>
+        </RadioGroup>
+       <button type="submit" >Submit</button>
+      </FormControl>
       </form>
         )
     }
--- frontend/src/containers/BusinessQuestions.js
+++ frontend/src/containers/BusinessQuestions.js
@@ -4,6 +4,14 @@ import EditQuestion from '../components/EditQuestion'
 import GoalList from "../components/GoalList"
 import { Button } from '@material-ui/core';
 
+// css
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+
 
 
 const API = 'http://localhost:3000/businesses'
@@ -17,7 +25,7 @@ class BusinessQuestions extends React.Component {
         answers: '',
         goals: [],
         currentQuestion: null,
-        currentGoal: {id: null, value: null}
+        currentGoal: {id: null, value: null, status: "Not Started"}
     }
 
     componentDidMount() {
@@ -62,23 +70,33 @@ class BusinessQuestions extends React.Component {
                 const currentId = this.state.currentQuestion.id
                 let goalList = [...this.state.goals]
                 let foundGoals = goalList.filter(goal => goal.business_question_answer_id === currentId)
-                return foundGoals.map(foundGoal => <GoalList key={foundGoal.id} goal={foundGoal} onUpdateGoal={this.updateGoal} onHandlePatch={this.handlePatch} />)
+                return foundGoals.map(foundGoal => <GoalList key={foundGoal.id} goal={foundGoal} onUpdateGoal={this.updateGoal} onUpdateStatus={this.updateStatus} onHandlePatch={this.handlePatch} />)
             }
             return null
         }
 
-        updateGoal = (e, newId) => {
+        updateGoal = (e, newGoal) => {
+            let selectedGoal = this.state.currentGoal
             this.setState({
                 currentGoal: {
-                    id: newId,
+                    ...selectedGoal,
+                    id: newGoal.id,
                     value: e.target.value
                 }
             })
         }
 
-        handlePatch = () => {
-            console.log(this.state.currentGoal.value)
+        updateStatus = (e) => {
+            let selectedGoal = this.state.currentGoal
+            this.setState({
+                currentGoal: {
+                    ...selectedGoal,
+                    status: e.target.value
+                }
+            })
+        }
 
+        handlePatch = () => {
             const configObject = {
                 method: "PATCH",
                 headers: {
@@ -86,7 +104,8 @@ class BusinessQuestions extends React.Component {
                     Accept: "application/json"
                 },
                 body: JSON.stringify({
-                    "steps": this.state.currentGoal.value
+                    steps: this.state.currentGoal.value,
+                    status: this.state.currentGoal.status
                 })
             }
 
@@ -99,23 +118,31 @@ class BusinessQuestions extends React.Component {
     render() {
         return (
             <div>
-            <table className={classes.table}>
-            <tbody>
-                <tr>
-                <th>
-                    <h3>Category</h3>
-                </th>
-                <th>
-                    <h3>Question</h3>
-                </th>
-                <th>
-                    <h3>Answer</h3>
-                </th>
-                </tr>
+            <Table>
+                <TableHead className={"table"} aria-label="spanning table" style={{backgroundColor: '#4C76B5', color: 'black',}}>
+                    <TableCell align="center" colSpan={4} style={{backgroundColor: 'black', fontSize: 45, color: 'white'}}>
+                            Company Profile
+                    </TableCell>
+                    <TableRow>
+                        <TableCell align="left" >
+                            Category
+                        </TableCell>
+                        <TableCell align="center">
+                            Question
+                        </TableCell>
+                        <TableCell align="left">
+                            Answer
+                        </TableCell>
+                        <TableCell align="left">
+                            Goals
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+            <TableBody style={{backgroundColor:'#AECAF2', color: 'black',}} >
                 {this.renderQuestions()}
-            </tbody>
-            </table>
-            {this.renderGoals()}
+            </TableBody>
+            </Table>
+                {this.renderGoals()}
             </div>
         );
     }
